04ecf74515fe942579ef519ae7e2b6d59c4346ca
Zouwen <zouweninthesky@yandex.ru>
2020-10-21 15:49:17 +0300

    Подготавливает проект к первой проверке

--- source/js/main.js
+++ source/js/main.js
@@ -6,10 +6,10 @@ var openMenu = function () {
   if (nav.classList.contains('main-nav--closed')) {
     nav.classList.remove('main-nav--closed');
     window.scrollTo(0, 0);
-    document.body.style.overflow = 'hidden';
+    document.body.classList.add('hidden');
   } else {
     nav.classList.add('main-nav--closed');
-    document.body.style.overflow = null;
+    document.body.classList.remove('hidden');
   }
 };
 
